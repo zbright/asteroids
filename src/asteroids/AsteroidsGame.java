686a438bd1cdbb335cbc670f29467820bff85b13
zbright <zachb@angieslist.com>
2013-12-04 23:30:27 -0500

    Bit more work setting up a framework. More to come

--- src/asteroids/AsteroidsGame.java
+++ src/asteroids/AsteroidsGame.java
@@ -9,6 +9,8 @@ import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
+import com.sun.corba.se.impl.oa.poa.ActiveObjectMap.Key;
+
 public class AsteroidsGame extends JFrame implements Runnable, KeyListener{
 	Thread thread;
 	static int screenWidth;
@@ -17,6 +19,7 @@ public class AsteroidsGame extends JFrame implements Runnable, KeyListener{
 	public AsteroidsGame() {
 		thread = new Thread();
 		thread.start();
+		super.setTitle("Asteroids");
 	}
 
 	public void paint(Graphics g) {
@@ -28,31 +31,65 @@ public class AsteroidsGame extends JFrame implements Runnable, KeyListener{
 			repaint();
 		}
 	}
+	
+	public void update(Graphics gfx) {
+		paint(gfx);
+	}
+
+	public void keyPressed(KeyEvent event) {
+		KeyPressRelease(event, true);
+	}
+	
+	public void keyReleased(KeyEvent event) {
+		KeyPressRelease(event, false);
+	}
+
+	public void keyTyped(KeyEvent event) {
+		//Nothing needed here
+	}
+	
+	private void KeyPressRelease(KeyEvent event, boolean press) {
+		int keyCode = event.getKeyCode();
+		
+		switch(keyCode)
+		{
+			//Player 1 movements
+			case(KeyEvent.VK_UP):
+				break;
+			case(KeyEvent.VK_LEFT):
+				break;
+			case(KeyEvent.VK_RIGHT):
+				break;
+			case(KeyEvent.VK_EQUALS): //Firing
+				break;
+			
+			//Player 2 movements
+			case(KeyEvent.VK_W):
+				break;
+			case(KeyEvent.VK_S):
+				break;
+			case(KeyEvent.VK_D):
+				break;
+			case(KeyEvent.VK_SPACE):
+				break;
+			
+			case(KeyEvent.VK_ENTER): //Start game after pause
+				break;
+			case(KeyEvent.VK_ESCAPE): //Bring up pause menu
+				break;
+			case(KeyEvent.VK_END): //Exit
+				break;
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		AsteroidsGame asteroids = new AsteroidsGame();
 		asteroids.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		screenWidth = screenSize.width - 50;
-		screenHeight = screenSize.height - 50;
+		screenWidth = screenSize.width;
+		screenHeight = screenSize.height;
 		asteroids.setSize(screenWidth, screenHeight);
 		asteroids.setVisible(true);
-	}
-
-	@Override
-	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
 	}
 }
